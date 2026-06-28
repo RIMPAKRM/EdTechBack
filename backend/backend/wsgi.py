5680a5513a5926785b86292a8798fabaef7ef023
RIMPAKRM <m.kashak@ya.ru>
2026-06-29 00:11:15 +0500

    Исправить путь к настройкам Django в asgi.py, wsgi.py и manage.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 
 application = get_wsgi_application()
