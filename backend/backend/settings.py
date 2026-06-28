bfedd592793c3f36e01d3d1fb4ca53ec9d9fd73a
RIMPAKRM <m.kashak@ya.ru>
2026-06-28 23:10:54 +0500

    Изменить настройки DEBUG и ALLOWED_HOSTS для повышения безопасности

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0p!4%h@r%$=xez&gyzujjb1&*q)m(!*a&rx5!&a-00w9ggvwgr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
