aeeeb8d2bdb8a73d9b376e91213cea003ea6abe9
RIMPAKRM <m.kashak@ya.ru>
2026-06-29 00:15:49 +0500

    Исправить пути к конфигурации URL и WSGI в settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'backend.backend.urls'
 
 TEMPLATES = [
     {
@@ -61,7 +61,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'backend.backend.wsgi.application'
 
 
 REST_FRAMEWORK = {
