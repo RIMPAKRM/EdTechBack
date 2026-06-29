60dd0201aaed25ba5aa158b613c52b9605da3d3f
RIMPAKRM <m.kashak@ya.ru>
2026-06-29 10:49:15 +0500

    Добавить новый домен в CORS_ALLOWED_ORIGINS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,6 +144,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "https://*.vercel.app",
     "https://ed-tech-front-7uvav5f62-maksims-projects-c61c7abb.vercel.app",
+    "https://ed-tech-front-three.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
